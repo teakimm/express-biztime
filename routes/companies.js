0d3a21d6069ee80c7db166a7558d09a89473bfcc
JiHye Yoon <yoonjihyeoi@gmail.com>
2024-03-21 12:29:50 -0700

    companies routes

--- routes/companies.js
+++ routes/companies.js
@@ -7,21 +7,102 @@ const router = new express.Router();
 const { BadRequestError, NotFoundError } = require("../expressError");
 
 
-/** (Fixed) Search by user type.
- *  Returns {users: [user, user, user] }
- *  Where user = {id, name, type}
+/** Gets companies
+ *  Returns JSON {companies: [{code, name}, ...]}
+ *  Where company = {code, name, description}
  */
 
-router.get("/companies",
+router.get("/",
 async function (req, res, next) {
-  const type = req.query.type;
-
   const results = await db.query(
-    `SELECT code, name,
-             FROM companies,
-             WHERE type = $1`, [type]);
+    `SELECT code, name
+             FROM companies`);
+
   const companies = results.rows;
   return res.json({ companies });
+});
+
+
+/** Search by company code.
+ *  Returns JSON {company: {code, name, description}}
+ *  Where company = {code, name, description}
+ */
+
+router.get("/:code",
+async function (req, res, next) {
+  const code = req.params.code
+
+  const results = await db.query(
+    `SELECT code, name, description
+             FROM companies
+             WHERE code = $1`, [code]);
+
+  const company = results.rows[0];
+
+  //TODO: if not company doesnt exist, 404 error
+
+  return res.json({ company });
+});
+
+
+/** Create new company,
+ * input requires JSON { "name": name, "description": description }
+ * returning JSON {company: {code, name, description}}
+*/
+
+router.post("/", async function (req, res, next) {
+  console.log ("*** POST / req.body:", req.body);
+  if (!req.body) throw new BadRequestError();
+
+  const { code, name, description } = req.body;
+  const result = await db.query(
+    `INSERT INTO companies (code, name, description)
+           VALUES ($1, $2, $3)
+           RETURNING code, name, description`,
+    [code, name, description],
+  );
+
+  const company = result.rows[0];
+  return res.status(201).json({ company });
+});
+
+
+/** Update company,
+ * input requires JSON { "name": name, "description": description }
+ * returning JSON {company: {code, name, description}}
+*/
+
+router.put("/:code", async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
+  const { name, description } = req.body;
+
+  const result = await db.query(
+    `UPDATE companies
+           SET name=$1,
+               description=$2
+           WHERE code = $3
+           RETURNING code, name, description`,
+    [name, description, req.params.code],
+    // RETURNING lets me use it later
+  );
+
+  const company = result.rows[0];
+
+  //TODO: if not company doesnt exist, 404 error
+
+  return res.json({ company });
+});
+
+/** Delete company, returning {status: "Deleted"} */
+
+router.delete("/:code", async function (req, res, next) {
+  await db.query(
+    "DELETE FROM companies WHERE code = $1",
+    // NEED TO RETURN
+    [req.params.code],
+  );
+
+  return res.json({ status: "deleted" });
 });
 
 
